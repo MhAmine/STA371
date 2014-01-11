c9940fe06154c5003ae5a9b343a9316664dc24c9
James Scott <james.scott@mccombs.utexas.edu>
2014-01-11 10:49:51 -0600

    directory enumeration

---
+++ _data/makeyml.R
@@ -0,0 +1,26 @@
+# R files
+
+readr = list.files("../r/")
+writer = "r.yml"
+
+myfile = readr[1]
+cat("- r: ", myfile, "\n", sep='', file= writer)
+
+for(i in 2:length(readr)) {
+	myfile = readr[i]
+	cat("- r: ", myfile, "\n", sep='', file= writer, append=TRUE)
+}
+
+
+# Data files
+
+readdat = list.files("../data/")
+writedat = "data.yml"
+
+myfile = readdat[1]
+cat("- data: ", myfile, "\n", sep='', file= writedat)
+
+for(i in 2:length(readdat)) {
+	myfile = readdat[i]
+	cat("- data: ", myfile, "\n", sep='', file= writedat, append=TRUE)
+}
